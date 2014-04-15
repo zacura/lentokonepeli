c69ee25db16dbd3d5620ef109224d885cbc06279
Tuomas <tuomas@ubuntu.(none)>
2014-04-15 15:17:44 +0300

    hitbox is now 15px smaller

--- JS/main.js
+++ JS/main.js
@@ -16,15 +16,14 @@ function init(){
         create: function() { 
             // Fuction called after 'preload' to setup the game
             // Display the bird on the screen
-            
             this.bg_clouds = game.add.sprite(0, 0, 'bg_clouds');
             this.bird = this.game.add.sprite(100, 245, 'rollator');
-            
             this.pipes = game.add.group();  
             this.pipes.createMultiple(20, 'pill');
             
             
             // Add gravity to the bird to make it fall
+            this.bird.body.setRectangle(50,50,7.5,7.5);
             this.bird.body.gravity.y = 1000;
             
             // Call the 'jump' function when the spacekey is hit
@@ -41,7 +40,7 @@ function init(){
     
         update: function() {
             // Function called 60 times per second
-            // If the bird is out of the world (too high or too low), call the 'restart_game' function
+            // If the bird is out of the world (too high or too low), call the 'restart_game' function  
             if (this.bg_clouds.x <-700) {
                 this.bg_clouds.x = 0;
             }
@@ -83,7 +82,7 @@ function init(){
         },
         
         add_row_of_pipes: function() {  
-        var hole = Math.floor(Math.random()*5)+2;
+        var hole = Math.floor(Math.random()*5)+1;
         this.score += 1;  
         this.label_score.content = this.score;  
         
@@ -95,4 +94,61 @@ function init(){
     };
     game.state.add('main',main_state);
     game.state.start('main');
+}
+
+function isPixelCollision( first, other, isCentred ){
+    var i = 0;
+    
+    for(i = 0; i < other.length;i++){
+    // we need to avoid using floats, as were doing array lookups
+    x  = Math.round( first.position.x );
+    y  = Math.round( first.position.y );
+    x2 = Math.round( other.getAt(i).position.x );
+    y2 = Math.round( other.getAt(i).position.y );
+
+    var w  = first.width,
+        h  = first.height,
+        w2 = other.width,
+        h2 = other.height ;
+
+    // deal with the image being centred
+    if ( isCentred ) {
+        // fast rounding, but positive only
+        x  -= ( w/2 + 0.5) << 0
+        y  -= ( h/2 + 0.5) << 0
+        x2 -= (w2/2 + 0.5) << 0
+        y2 -= (h2/2 + 0.5) << 0
+    }
+    
+    // find the top left and bottom right corners of overlapping area
+    var xMin = Math.max( x, x2 ),
+        yMin = Math.max( y, y2 ),
+        xMax = Math.min( x+w, x2+w2 ),
+        yMax = Math.min( y+h, y2+h2 );
+    
+    // Sanity collision check, we ensure that the top-left corner is both
+    // above and to the left of the bottom-right corner.
+    if ( xMin >= xMax || yMin >= yMax ) {
+        return false;
+    }
+    
+    var xDiff = xMax - xMin,
+    yDiff = yMax - yMin;
+
+    // get the pixels out from the images
+    var pixels  = first.data,
+        pixels2 = other.data;
+    //the actual comparison
+    for ( var pixelX = xMin; pixelX < xMax; pixelX++ ) {
+        for ( var pixelY = yMin; pixelY < yMax; pixelY++ ) {
+        if (
+        ( pixels [ ((pixelX-x ) + (pixelY-y )*w )*4 + 3 ] !== 0 ) &&
+        ( pixels2[ ((pixelX-x2) + (pixelY-y2)*w2)*4 + 3 ] !== 0 )
+        ) {
+            return true;
+        }
+        }
+    }
+    return false;
+    }
 }
